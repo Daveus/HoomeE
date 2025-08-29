83b0bacf0121744328afca5ba4f3c533470d7cdb
Christian Marlats <marlats.christian@gmail.com>
2025-08-29 13:36:44 +0200

    nuova idea card v1

--- src/app/shared-components/header/header.component.ts
+++ src/app/shared-components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
